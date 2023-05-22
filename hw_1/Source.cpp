2e1f88aea0c172b8bdc23a18308f8db356a689e5
Roman <roma2000kuba@gmail.com>
2023-05-22 23:32:26 +0300

    ex_2 done

---
+++ hw_1/Source.cpp
@@ -0,0 +1,16 @@
+﻿﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string poem = "Kraj lukomor’a dub zelenyj,\n"
+        "I zolotyj lancyuh na nim:\n"
+        "Shodnya, shonochi kit uchenyj\n"
+        "Na lancyuhu kruzhl’aje tim";
+
+    cout << poem << endl << "\a\a\a" << endl;
+
+    return 0;
+}
